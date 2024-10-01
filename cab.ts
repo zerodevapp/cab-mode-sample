aa31827d49699b71b61ab111900eb6f6aa22b48f
Sahil Vasava <sahilvasava@gmail.com>
2024-10-01 18:28:19 +0700

    chore: cleanup

--- cab.ts
+++ cab.ts
@@ -1,41 +1,52 @@
-import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from "@zerodev/sdk"
-import { KERNEL_V3_1 } from "@zerodev/sdk/constants"
-import { toMultiChainECDSAValidator } from "@zerodev/multi-chain-validator"
-import { http, createPublicClient } from "viem"
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
-import { mode } from "viem/chains"
-import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from "permissionless"
-import { createKernelCABClient, CAB_V0_1 } from '@zerodev/cab';
-import dotenv from 'dotenv'
-dotenv.config()
+import {
+  createKernelAccount,
+  createKernelAccountClient,
+  createZeroDevPaymasterClient,
+} from "@zerodev/sdk";
+import { KERNEL_V3_1 } from "@zerodev/sdk/constants";
+import { toMultiChainECDSAValidator } from "@zerodev/multi-chain-validator";
+import {
+  http,
+  createPublicClient,
+  parseUnits,
+  encodeFunctionData,
+  erc20Abi,
+  Address,
+} from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { mode } from "viem/chains";
+import { ENTRYPOINT_ADDRESS_V07, bundlerActions } from "permissionless";
+import { createKernelCABClient, CAB_V0_1 } from "@zerodev/cab";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PROJECT_ID = process.env.ZERODEV_MODE_ID;
-const BUNDLER_RPC = `https://rpc.zerodev.app/api/v2/bundler/${PROJECT_ID}?provider=ZERODEV`
-const PAYMASTER_RPC = `https://rpc.zerodev.app/api/v2/paymaster/${PROJECT_ID}?provider=ZERODEV`
+const BUNDLER_RPC = `https://rpc.zerodev.app/api/v2/bundler/${PROJECT_ID}?provider=ZERODEV`;
+const PAYMASTER_RPC = `https://rpc.zerodev.app/api/v2/paymaster/${PROJECT_ID}?provider=ZERODEV`;
 const CAB_PAYMASTER_RPC = process.env.CAB_PAYMASTER_RPC;
- 
-const chain = mode
-const entryPoint = ENTRYPOINT_ADDRESS_V07
-const kernelVersion = KERNEL_V3_1
- 
+
+const chain = mode;
+const entryPoint = ENTRYPOINT_ADDRESS_V07;
+const kernelVersion = KERNEL_V3_1;
+
 const main = async () => {
   // Construct a signer
   const privateKey = generatePrivateKey()
-  const signer = privateKeyToAccount(privateKey)
- 
+  const signer = privateKeyToAccount(privateKey);
+
   // Construct a public client
   const publicClient = createPublicClient({
     chain,
     transport: http(BUNDLER_RPC),
-  })
- 
+  });
+
   // Construct a validator
   const ecdsaValidator = await toMultiChainECDSAValidator(publicClient, {
     signer,
     entryPoint,
-    kernelVersion
-  })
- 
+    kernelVersion,
+  });
+
   // Construct a Kernel account
   const account = await createKernelAccount(publicClient, {
     plugins: {
@@ -43,9 +54,9 @@ const main = async () => {
     },
     entryPoint,
     kernelVersion,
-    useMetaFactory: false
-  })
- 
+    useMetaFactory: false,
+  });
+
   // Construct a Kernel account client
   const kernelClient = createKernelAccountClient({
     account,
@@ -58,49 +69,62 @@ const main = async () => {
           chain,
           entryPoint,
           transport: http(PAYMASTER_RPC),
-        })
+        });
         return zerodevPaymaster.sponsorUserOperation({
           userOperation,
           entryPoint,
-        })
+        });
       },
     },
-  }) 
+  });
+  const bundlerClient = kernelClient.extend(bundlerActions(entryPoint));
 
   const cabClient = createKernelCABClient(kernelClient, {
     transport: http(CAB_PAYMASTER_RPC),
     entryPoint,
-    cabVersion: CAB_V0_1
-  })
+    cabVersion: CAB_V0_1,
+  });
 
   // enable CAB
   await cabClient.enableCAB({
-    tokens: [{ name: "USDC", networks: [34443] }]
-  })
- 
+    tokens: [{ name: "USDC", networks: [mode.id] }],
+  });
+
   // Send a UserOp
   // mode USDC
-  // const sponsorTokens = [
-  //   {
-  //     address: '0xd988097fb8612cc24eeC14542bC03424c656005f' as Address,
-  //     amount: parseUnits('0.01', 6)
-  //   }
-  // ]
-  // const calls = sponsorTokens.map(({ address, amount }) => ({
-  //   to: address,
-  //   data: encodeFunctionData({
-  //     abi: erc20Abi,
-  //     functionName: "transfer",
-  //     args: [kernelClient.account.address, amount]
-  //   }),
-  //   value: 0n
-  // }))
+  const sponsorTokens = [
+    {
+      address: "0xd988097fb8612cc24eeC14542bC03424c656005f" as Address,
+      amount: parseUnits("0.01337", 6),
+    },
+  ];
+  const calls = sponsorTokens.map(({ address, amount }) => ({
+    to: address,
+    data: encodeFunctionData({
+      abi: erc20Abi,
+      functionName: "transfer",
+      args: [kernelClient.account.address, amount],
+    }),
+    value: 0n,
+  }));
 
-  // const userOperation = await cabClient.prepareUserOperationRequestCAB({
-  //   calls,
-  //   repayTokens: ["USDC"]
-  // })
-  // console.log('userOperation', userOperation);
-}
- 
-main()
+  const { userOperation } = await cabClient.prepareUserOperationRequestCAB({
+    calls,
+    repayTokens: ["USDC"],
+  });
+  console.log("userOperation", userOperation);
+
+  const userOpHash = await cabClient.sendUserOperationCAB({ userOperation });
+  console.log("userOpHash", userOpHash);
+  const ret = await bundlerClient.waitForUserOperationReceipt({
+    hash: userOpHash,
+    timeout: 100_000_000,
+  });
+  console.log(
+    "transfer with cab transactionHash:",
+    ret.receipt.transactionHash
+  );
+
+};
+
+main();
